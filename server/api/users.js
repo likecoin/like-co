ae64811a39094c9dbbaa8eb0ef98f87961f65295
William Chong <williamchong007@msn.com>
2018-01-05 18:17:46 +0800

    + Add new user api

--- server/api/users.js
+++ server/api/users.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+const Account = require("eth-lib/lib/account");
 
 const router = Router()
 
@@ -15,6 +16,28 @@ const USERS = {
     avatar: 'https://likecoin.foundation/static/img/kin.749505b.png',
   },
 }
+
+/* GET user by ID. */
+router.put("/users/new", async (req, res) => {
+  try {
+    const {from, payload, sign} = req.body;
+    const recovered = Account.recover(payload, sign);
+    if (recovered.toLowerCase() !== from.toLowerCase()) {
+      throw "recovered address not match";
+    }
+    const { user, displayName, wallet, avatar } = JSON.parse(payload);
+    if (from !== wallet || USERS[user]) {
+      res.sendStatus(400);
+      return;
+    }
+    USERS[user] = { displayName, wallet, avatar };
+    res.status(200);
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
 
 /* GET user by ID. */
 router.get('/users/:id', function (req, res, next) {
