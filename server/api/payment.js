4894c4a5bb161e148f0f864279c2bb6e879e9985
William Chong <williamchong007@msn.com>
2018-01-10 12:18:59 +0800

    + Backport temp nonce fix from likecoin-coupon

--- server/api/payment.js
+++ server/api/payment.js
@@ -10,19 +10,34 @@ const router = Router();
 const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/ywCD9mvUruQeYcZcyghk'));
 const LikeCoin = new web3.eth.Contract(LIKECOIN.LIKE_COIN_ABI, LIKECOIN.LIKE_COIN_ADDRESS);
 const {
+  address,
   privateKey,
   gasPrice,
   gasLimit,
 } = accounts[0];
 
-function sendTransaction(tx, txHashCb) {
+function sendTransaction(tx) {
   return new Promise((resolve, reject) => {
     const txEventEmitter = web3.eth.sendSignedTransaction(tx.rawTransaction);
-    if (txHashCb) {
-      txEventEmitter.on('transactionHash', txHashCb);
-    }
-    txEventEmitter.on('confirmation', resolve).on('error', reject);
+    txEventEmitter.on('transactionHash', resolve)
+      .on('error', (err) => {
+        if (err.message === 'Returned error: replacement transaction underpriced'
+          || err.message.includes('Returned error: known transaction:')) resolve(false);
+        else reject(err);
+      });
   });
+}
+
+async function signTransaction(txData, count) {
+  const pendingCount = count;
+  const tx = await web3.eth.accounts.signTransaction({
+    to: LIKECOIN.LIKE_COIN_ADDRESS,
+    nonce: pendingCount,
+    data: txData,
+    gasPrice,
+    gas: gasLimit,
+  }, privateKey);
+  return tx;
 }
 
 router.post('/payment', async (req, res) => {
@@ -49,15 +64,15 @@ router.post('/payment', async (req, res) => {
       s,
     );
     const txData = methodCall.encodeABI();
-    const tx = await web3.eth.accounts.signTransaction({
-      to: LIKECOIN.LIKE_COIN_ADDRESS,
-      data: txData,
-      gasPrice,
-      gas: gasLimit,
-    }, privateKey);
-    await sendTransaction(tx, (txHash) => {
-      res.json({ txHash });
-    });
+    let txHash;
+    let pendingCount = await web3.eth.getTransactionCount(address, 'pending');
+    while (!txHash) {
+      // eslint-disable-next-line no-await-in-loop
+      const tx = await signTransaction(txData, pendingCount);
+      pendingCount += 1;
+      txHash = await sendTransaction(tx); // eslint-disable-line no-await-in-loop
+    }
+    res.json({ txHash });
   } catch (err) {
     console.error(err);
     const msg = err.message || err;
