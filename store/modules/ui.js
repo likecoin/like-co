a3778c1a58f4538e9f97c2d43e50573dc75031c0
Michael Cheung <mc.kingho@gmail.com>
2018-01-11 13:15:29 +0800

    ! Fix loading color

--- store/modules/ui.js
+++ store/modules/ui.js
@@ -4,6 +4,7 @@ import {
   UI_ERROR_ICON,
   UI_ERROR_MSG,
   UI_START_LOADING,
+  UI_START_LOADING_TX,
   UI_STOP_LOADING,
 } from '../mutation-types';
 import * as getters from './getters/ui';
@@ -12,6 +13,7 @@ import * as actions from './actions/ui';
 const state = {
   errorIcon: 'warning',
   errorMsg: '',
+  isInTransaction: false,
   isLoading: false,
 };
 
@@ -23,6 +25,11 @@ const mutations = {
     state.errorMsg = msg;
   },
   [UI_START_LOADING](state) {
+    state.isInTransaction = false;
+    state.isLoading = true;
+  },
+  [UI_START_LOADING_TX](state) {
+    state.isInTransaction = true;
     state.isLoading = true;
   },
   [UI_STOP_LOADING](state) {
