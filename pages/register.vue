2d84da397870434968ca8976ab953b4db9f9ffac
William Chong <williamchong007@msn.com>
2018-01-11 17:06:58 +0800

    = Refactor container and header out of pages

--- pages/register.vue
+++ pages/register.vue
@@ -1,55 +1,41 @@
 <template>
-  <div class="container">
-    <div class="landing">
-      <div class="upper-left-corner" />
-      <site-header />
-      <introduction />
-      <description />
+  <div class="inner-container">
+    <div id="has-account">
+      <nuxt-link :to="{ name: 'edit' }"> {{ hasAccountMsg }}</nuxt-link>
     </div>
-    <div class="section-title-wrapper">
-      <span class="title">{{ title }}</span>
+    <div v-if="isPreview" class="avatar-preview">
+      <md-card-media>
+        <img :src="avatarData" >
+      </md-card-media>
     </div>
-    <div class="inner-container">
-      <div id="has-account">
-        <router-link :to="{ name: 'register' }"> {{ hasAccountMsg }}</router-link>
-      </div>
-      <div v-if="isPreview" class="avatar-preview">
-        <md-card-media>
-          <img :src="avatarData" >
-        </md-card-media>
-      </div>
-      <form id="registerForm" v-on:submit.prevent="onSubmit">
-        <md-field>
-          <label>Image upload</label>
-          <md-file v-model="avatar" @md-change="previewImage($event)" accept="image/*"></md-file>
-        </md-field>
-        <md-field>
-          <label>Username</label>
-          <md-input v-model="user" required></md-input>
-        </md-field>
-        <md-field>
-          <label>Display Name</label>
-          <md-input v-model="displayName" required></md-input>
-        </md-field>
-        <md-field :class="isBadAddress?'md-invalid':''">
-          <label>ETH wallet address</label>
-          <md-input v-model="wallet" maxlength="42" required disabled />
-          <span v-if="isBadAddress" class="md-error">Invalid address format</span>
-        </md-field>
+    <form id="registerForm" v-on:submit.prevent="onSubmit">
+      <md-field>
+        <label>Image upload</label>
+        <md-file v-model="avatar" @md-change="previewImage($event)" accept="image/*"></md-file>
+      </md-field>
+      <md-field>
+        <label>Username</label>
+        <md-input v-model="user" required></md-input>
+      </md-field>
+      <md-field>
+        <label>Display Name</label>
+        <md-input v-model="displayName" required></md-input>
+      </md-field>
+      <md-field :class="isBadAddress?'md-invalid':''">
+        <label>ETH wallet address</label>
+        <md-input v-model="wallet" maxlength="42" required disabled />
+        <span v-if="isBadAddress" class="md-error">Invalid address format</span>
+      </md-field>
 
-        <hr />
-        <div id="form-btn">
-          <md-button class="md-raised" type="submit" form="registerForm">Submit</md-button>
-        </div>
-      </form>
-    </div>
+      <hr />
+      <div id="form-btn">
+        <md-button class="md-raised" type="submit" form="registerForm">Submit</md-button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import SiteHeader from '~/components/Header';
-import Introduction from '~/components/Introduction';
-import Description from '~/components/Description';
 import EthHelper from '@/util/EthHelper';
 import FileHelper from '@/util/FileHelper';
 import { mapActions } from 'vuex';
@@ -58,7 +44,6 @@ export default {
   name: 'Register',
   data() {
     return {
-      title: 'Create Account and Redeem',
       hasAccountMsg: 'I already have a LikeCoin account',
       avatarFile: null,
       avatar: null,
@@ -69,11 +54,6 @@ export default {
       isBadAddress: false,
       isPreview: false,
     };
-  },
-  components: {
-    SiteHeader,
-    Introduction,
-    Description,
   },
   methods: {
     ...mapActions([
@@ -153,97 +133,16 @@ export default {
 
 </script>
 
-<style lang="scss" scoped>
-@import "../styles/index.scss";
-
-$large-padding: 64px;
-$mid-padding: 24px;
-$small-padding: 16px;
-
-.container {
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  max-width: 1440px;
-  margin: 0 auto;
-
-  overflow: hidden;
-
-  > * {
-    margin-right: $large-padding;
-    margin-left: $large-padding;
-  }
+<style>
+#has-account {
+  text-align: right;
+}
+.md-card-media img {
+  width: auto;
+  max-width: 400px;
+}
+#form-btn {
+  text-align: right;
 }
 
-.landing {
-  z-index: 0;
-  position: relative;
-  .upper-left-corner {
-    position: absolute;
-    z-index: -1;
-
-    width: calc(66.66% + 64px);
-    height: calc(100% + 84px);
-
-    margin-left: -$large-padding;
-
-    background-color: $like-gray-1;
-  }
-}
-
-.section-title-wrapper {
-  margin-top: 60px;
-  z-index: 1;
-  display: inline-block;
-  padding: 0 80px;
-  text-align: center;
-  background-color: #d2f0f0;
-  width: 50%;
-}
-
-.inner-container {
-  #has-account {
-    text-align: right;
-  }
-  .md-card-media img {
-    width: auto;
-    max-width: 400px;
-  }
-  #form-btn {
-    text-align: right;
-  }
-}
-
-
-@media (max-width: 768px) {
-  body .container {
-    > * {
-      margin-right: $mid-padding;
-      margin-left: $mid-padding;
-    }
-    .landing .upper-left-corner {
-      height: calc(100% + 50px);
-      margin-left: -$mid-padding;
-    }
-    .section-title-wrapper {
-      width: 100%;
-    }
-  }
-}
-
-@media (max-width: 500px) {
-  body .container {
-    > * {
-      margin-right: $small-padding;
-      margin-left: $small-padding;
-    }
-
-    .landing .upper-left-corner {
-      height: calc(100% + 50px);
-      margin-left: -$small-padding;
-    }
-  }
-}
 </style>
