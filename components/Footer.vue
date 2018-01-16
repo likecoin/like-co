9671fd60b47747f82e508df85a7e925139137b76
William Chong <williamchong007@msn.com>
2018-01-16 18:01:18 +0800

    * Update temp footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer>
-    Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+    Visit our website for more information : <a href="https://likecoin.foundation/" target="_blank">likecoin.foundation</a>
   </footer>
 </template>
