98083ce6143b233cd7722592310ac37a5efbe842
William Chong <williamchong007@msn.com>
2018-02-07 17:16:59 +0800

    + Add console.log to distinguish deploy setting

--- server/index.js
+++ server/index.js
@@ -45,4 +45,5 @@ app.use(nuxt.render);
 
 // Listen the server
 app.listen(port, host);
+console.log(`Deploying on ${process.env.IS_TESTNET ? 'rinkeby' : 'mainnet'}`); // eslint-disable-line no-console
 console.log(`Server listening on ${host}:${port}`); // eslint-disable-line no-console
