6022935711cdc9e166d928fbe669b44ff46e150b
Ng Wing Tat, David <i@ngwingt.at>
2018-03-06 17:57:54 +0800

    * Update whitepaper page

---
+++ layouts/defaultWithHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="lc-page-wrapper">
+
+    <div class="lc-page-content">
+      <header class="lc-container-1">
+        <div class="lc-container-2">
+          <site-header/>
+        </div>
+      </header>
+
+      <nuxt/>
+    </div>
+
+    <my-footer/>
+
+  </div>
+</template>
+
+
+<script>
+import SiteHeader from '~/components/header/Header';
+import MyFooter from '~/components/footer/Footer';
+
+export default {
+  components: {
+    SiteHeader,
+    MyFooter,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "../assets/index";
+@import "../assets/default";
+
+.lc-page-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  height: 100%;
+
+  background-color: $like-gray-1;
+}
+
+.lc-container-2 #site-header {
+  padding-right: 0;
+  padding-left: 0;
+}
+</style>
