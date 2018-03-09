d0b44a575dfaf1d1b25e5f01f77a0425902a5043
Michael Cheung <mc.kingho@gmail.com>
2018-03-09 19:26:21 +0800

    + Add firebase and ses stub files

---
+++ test.note.md
@@ -0,0 +1,14 @@
+## express test
+- backup and replace with stub files
+- `npm run dev`
+- npm run test:express
+
+## nuxt test
+- npm run test:nuxt
+
+## testing framework
+- ava
+- supertest
+
+## ava
+- 'only one' e.g. test.only('eg', t => { t.is(1+2, 3) })
---
+++ test/stub/server/util/ses.js
@@ -0,0 +1,13 @@
+console.log('Using stub (ses.js)'); /* eslint no-console: "off" */
+
+export async function sendVerificationEmail() {
+  return Promise.resolve();
+}
+
+export async function sendVerificationWithCouponEmail() {
+  return Promise.resolve();
+}
+
+export async function sendWelcomeEmail() {
+  return Promise.resolve();
+}
