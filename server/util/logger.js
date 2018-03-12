9f3379c52bc77b1f9749d39055e8d3e043e9dd8d
William Chong <williamchong007@msn.com>
2018-03-12 17:55:27 +0800

    ! Fix missing logRegisterKYC

--- server/util/logger.js
+++ server/util/logger.js
@@ -14,6 +14,20 @@ export async function logTransferDelegatedTx(payload) {
   }
 }
 
+export async function logRegisterKYC(payload) {
+  const { txHash } = payload;
+  try {
+    await dbRef.doc(txHash).create({
+      type: 'logRegisterKYC',
+      status: 'pending',
+      ts: Date.now(),
+      ...payload,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 export async function logETHTx(payload) {
   const { txHash } = payload;
   try {
