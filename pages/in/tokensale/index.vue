92d88eb032b1f0d6f6775ca3a959721e81a27038
William Chong <williamchong007@msn.com>
2018-03-23 16:37:15 +0800

    - Remove LIKE href in tokensale

--- pages/in/tokensale/index.vue
+++ pages/in/tokensale/index.vue
@@ -72,12 +72,9 @@
                     <li>
                       <div>
                         <span class="label">{{ $t('TokenSale.label.token') }}</span>
-                        <a
-                          class="value highlight"
-                          :href="LIKE_CONTRACT_ADDRESS"
-                          target="_blank">
+                        <span class="value highlight" >
                           LIKE
-                        </a>
+                        </span>
                       </div>
                     </li>
                     <li>
