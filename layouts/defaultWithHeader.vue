3c61b20bd0e8e8846f0a14373bbe55949497954e
Miles Wong <miles@oice.com>
2018-03-27 17:23:30 +0800

    * Update layout of defaultWithHeader

--- layouts/defaultWithHeader.vue
+++ layouts/defaultWithHeader.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="lc-page-wrapper">
+    <header class="lc-page-header">
+
+      <div class="lc-container-0 header-bg lc-narrow">
+        <div class="lc-container-1">
+          <div class="lc-container-2">
+            <div class="lc-container-3" />
+          </div>
+        </div>
+      </div>
+
+      <div class="lc-container-0">
+        <div class="lc-container-1">
+          <div class="underlay gray md-xsmall-hide" />
+          <div class="lc-container-2">
+            <site-header/>
+          </div>
+        </div>
+      </div>
+
+    </header>
 
     <div class="lc-page-content">
-      <header class="lc-container-1">
-        <div class="underlay gray md-xsmall-hide" />
-        <div class="lc-container-2">
-          <site-header/>
-        </div>
-      </header>
-
       <nuxt/>
     </div>
 
-    <my-footer/>
+    <footer class="lc-page-footer">
+      <div class="lc-container-0">
+        <my-footer/>
+      </div>
+    </footer>
 
   </div>
 </template>
@@ -41,6 +58,10 @@ export default {
   justify-content: space-between;
 
   height: 100%;
+
+  .lc-page-footer {
+    padding: 0;
+  }
 }
 
 .lc-container-2 #site-header {
