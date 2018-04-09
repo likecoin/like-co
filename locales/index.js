1394596298a69526c49881be70bd844e1637d572
Simonb the game designer <simonbut711@gmail.com>
2018-04-09 16:38:16 +0800

    + Add language : de, es, fr, it, ru, pt

--- locales/index.js
+++ locales/index.js
@@ -1,8 +1,18 @@
+import de from './de.json';
 import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import it from './it.json';
+import ru from './ru.json';
 import zh from './zh.json';
 
 const messages = {
+  de,
   en,
+  es,
+  fr,
+  it,
+  ru,
   zh,
 };
 
