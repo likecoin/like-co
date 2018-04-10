4d4e91e79b18d24784fe7a9cab8f0b03c169d7c6
Chung <chung@lakoo.com>
2018-04-10 17:00:08 +0800

    + Add oEmbed test cases

--- test/express.test.js
+++ test/express.test.js
@@ -358,3 +358,90 @@ test('KYC: Advanced KYC, User wallet not match case', async (t) => {
   t.is(res.status, 400);
   t.is(res.data, 'User wallet not match');
 });
+
+test('OEMBED: success cases', async (t) => {
+  let res;
+
+  res = await axios.get(`${url}/api/oembed?url=https://rinkeby.like.co/${testingUser1}`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingDisplayName1} (${testingUser1})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser1}`);
+  t.is(res.data.thumbnail_width, 100);
+  t.is(res.data.thumbnail_height, 100);
+
+  res = await axios.get(`${url}/api/oembed?url=http://rinkeby.like.co/${testingUser1}`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingDisplayName1} (${testingUser1})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser1}`);
+  t.is(res.data.thumbnail_width, 100);
+  t.is(res.data.thumbnail_height, 100);
+
+  res = await axios.get(`${url}/api/oembed?url=rinkeby.like.co/${testingUser1}`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingDisplayName1} (${testingUser1})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser1}`);
+  t.is(res.data.thumbnail_width, 100);
+  t.is(res.data.thumbnail_height, 100);
+
+  res = await axios.get(`${url}/api/oembed?url=www.rinkeby.like.co/${testingUser1}`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingDisplayName1} (${testingUser1})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser1}`);
+  t.is(res.data.thumbnail_width, 100);
+  t.is(res.data.thumbnail_height, 100);
+
+  res = await axios.get(`${url}/api/oembed?url=https://www.rinkeby.like.co/${testingUser1}`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingDisplayName1} (${testingUser1})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser1}`);
+  t.is(res.data.thumbnail_width, 100);
+  t.is(res.data.thumbnail_height, 100);
+
+  res = await axios.get(`${url}/api/oembed?url=https://rinkeby.like.co/${testingUser2}&maxwidth=50`)
+    .catch(err => err.response);
+  t.is(res.status, 200);
+  t.is(res.data.type, 'link');
+  t.is(res.data.title, `${testingUser2} (${testingUser2})`);
+  t.is(res.data.version, '1.0');
+  t.is(res.data.url, `https://rinkeby.like.co/${testingUser2}`);
+  t.is(res.data.thumbnail_width, 50);
+  t.is(res.data.thumbnail_height, 50);
+});
+
+test('OEMBED: failure cases', async (t) => {
+  let res;
+
+  res = await axios.get(`${url}/api/oembed?url=https://rinkeby.like.co/nosuchuser`)
+    .catch(err => err.response);
+  t.is(res.status, 404);
+
+  res = await axios.get(`${url}/api/oembed`)
+    .catch(err => err.response);
+  t.is(res.status, 400);
+  t.is(res.data, 'No url query in oEmbed request');
+
+  res = await axios.get(`${url}/api/oembed?url=www.invalidurl.like.co/testing`)
+    .catch(err => err.response);
+  t.is(res.status, 400);
+  t.is(res.data, 'Invalid url query (www.invalidurl.like.co/testing) in oEmbed request');
+
+  res = await axios.get(`${url}/api/oembed?url=https://rinkeby.like.co/${testingUser1}&format=nosuchformat`)
+    .catch(err => err.response);
+  t.is(res.status, 400);
+  t.is(res.data, 'Invalid format nosuchformat in oEmbed request');
+});
