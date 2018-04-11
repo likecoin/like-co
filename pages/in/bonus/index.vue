6ecb89c7d716e1d218a8fa8bcad984a50a0551c2
Ng Wing Tat, David <i@ngwingt.at>
2018-04-11 16:40:51 +0800

    + Add bonus pages

---
+++ pages/in/bonus/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="bonus-tab">
+
+    <!-- Description -->
+    <div class="lc-container-0">
+      <div class="lc-container-1">
+        <div class="lc-container-2">
+          <div class="lc-container-3">
+            <div class="lc-container-4">
+              <div class="lc-width-2-3 lc-margin-top-32 lc-padding-vertical-8 lc-font-size-18 lc-color-like-gray-4 lc-mobile">
+                {{ $t('BonusPage.label.description') }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- BEGIN - LikeCoin Bonus Section -->
+    <section class="likecoin-bonus-section lc-margin-top-48 lc-mobile">
+      <div class="lc-container-0">
+        <div class="lc-container-1">
+
+          <div class="lc-container-header">
+            <div class="lc-container-2 lc-container-header-overlay">
+              <div class="lc-container-3 lc-bg-gray-1" />
+            </div>
+            <div class="lc-container-2">
+              <div class="lc-container-3">
+                <div class="lc-container-4">
+                  <div class="lc-container-header-title">
+                    <h1 class="lc-font-size-32 lc-mobile">
+                      {{ $t('BonusPage.header.likeCoinBonus') }}
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="lc-container-2">
+            <div class="lc-container-3 lc-bg-gray-1 lc-padding-vertical-32 section-content">
+              <div class="lc-container-4">
+                List of Bonus
+              </div>
+            </div>
+          </div>
+
+          <div class="lc-container-2 lc-margin-top-4" id="invitees-task">
+            <div class="lc-container-3 lc-bg-gray-1 lc-padding-vertical-32 section-content">
+              <div class="lc-container-4">
+                Invitees Task
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!-- END - LikeCoin Bonus Section -->
+
+    <!-- BEGIN - Referral Section -->
+    <section class="referral-form-section lc-margin-top-48 lc-mobile" id="referral">
+      <div class="lc-container-0">
+        <div class="lc-container-1">
+
+          <div class="lc-container-header">
+            <div class="lc-container-2 lc-container-header-overlay">
+              <div class="lc-container-3 lc-bg-gray-1" />
+            </div>
+            <div class="lc-container-2">
+              <div class="lc-container-3">
+                <div class="lc-container-4">
+                  <div class="lc-container-header-title">
+                    <h1 class="lc-font-size-32 lc-mobile">
+                      {{ $t('Edit.referral.title') }}
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="lc-container-2">
+            <div class="lc-container-3 lc-bg-gray-1 lc-padding-vertical-32 section-content">
+              <div class="lc-container-4">
+                <referral-action
+                  :user="user"
+                  :pending="referralPending"
+                  :verified="referralVerified"
+                  :isEmailVerified="getUserInfo.isEmailVerified"
+                  :isBlocked="getIsPopupBlocking"
+                />
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!-- END - Referral Section -->
+
+  </div>
+</template>
+
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+import ReferralAction from '@/components/ReferralAction';
+
+export default {
+  name: 'bonus-index',
+  data() {
+    return {
+      referralPending: 0,
+      referralVerified: 0,
+      user: '',
+    };
+  },
+  components: {
+    ReferralAction,
+  },
+  computed: {
+    ...mapGetters([
+      'getIsPopupBlocking',
+      'getUserInfo',
+      'getUserIsFetching',
+      'getUserIsRegistered',
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'fetchUserReferralStats',
+    ]),
+    async updateInfo() {
+      const user = this.getUserInfo;
+      this.user = user.user;
+      this.updateReferralStat();
+    },
+    async updateReferralStat() {
+      try {
+        const { pending, verified } = await this.fetchUserReferralStats(this.user);
+        this.referralPending = pending;
+        this.referralVerified = verified;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  watch: {
+    getUserIsFetching(f) {
+      if (!f) {
+        if (this.getUserIsRegistered) {
+          this.updateInfo();
+        }
+      }
+    },
+  },
+  mounted() {
+    if (!this.getUserIsFetching) {
+      if (this.getUserIsRegistered) {
+        this.updateInfo();
+      }
+    }
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "~assets/variables";
+
+.bonus-tab {
+}
+</style>
