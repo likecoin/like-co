67e4ffce28ada3a601b9fb9b6fb3522a1dfe4437
William Chong <williamchong007@msn.com>
2018-04-12 13:35:31 +0800

    + Add mission complete shortcut in query

--- server/api/mission.js
+++ server/api/mission.js
@@ -15,23 +15,49 @@ const {
 
 const router = Router();
 
+async function checkAlreadyDone(m, { u, doneList }) {
+  const { id } = m;
+  const mission = m.data();
+  const username = u.id;
+  const user = u.data();
+  let isDone = false;
+  switch (id) {
+    case 'verifyEmail': {
+      if (user.isEmailVerified) isDone = true;
+      break;
+    }
+    default: return true;
+  }
+  if (!isDone) return true;
+  const payload = { done: true };
+  doneList.push(id);
+  if (!mission.reward) payload.bonusId = 'none';
+  await dbRef.doc(username).collection('mission').doc(id).set(payload, { merge: true });
+  return !!(mission.reward);
+}
+
 router.get('/mission/list/:id', async (req, res) => {
   try {
     const username = req.params.id;
-    const missionCol = await missionsRef.get();
+    const missionCol = await missionsRef.orderBy('priority').get();
+    const userDoc = await dbRef.doc(username).get();
+    if (!userDoc.exists) throw new Error('user not exist');
     const userMissionCol = await dbRef.doc(username).collection('mission').get();
     const userMisionList = userMissionCol.docs.map(d => d.id);
     const missionDone = userMissionCol.docs.filter(d => d.data().done).map(d => d.id);
 
-    const replyMissionList = userMissionCol.docs.filter(d => !d.data().payoutId);
-    missionCol.forEach((m) => {
-      if (userMisionList.includes(m.id)) return;
-      let fullfilled = true;
-      m.data().require.forEach((r) => {
-        if (!missionDone.includes(r)) fullfilled = false;
-      });
-      if (fullfilled) replyMissionList.push(m);
-    });
+    const replyMissionList = userMissionCol.docs.filter(d => !d.data().bonusId);
+    for (let index = 0; index < missionCol.docs.length; index += 1) {
+      const m = missionCol.docs[index];
+      if (!userMisionList.includes(m.id)) {
+        const requires = m.data().require;
+        const fullfilled = requires.every(id => missionDone.includes(id));
+        // eslint-disable-next-line no-await-in-loop
+        if (fullfilled && (await checkAlreadyDone(m, { u: userDoc, doneList: missionDone }))) {
+          replyMissionList.push(m);
+        }
+      }
+    }
     const missions = replyMissionList.map(d => ({
       id: d.id,
       ...Validate.filterMissionData(d.data()),
