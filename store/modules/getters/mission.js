8e65cd2d5f20b81b7b44d22e59b21b4d190f9920
William Chong <williamchong007@msn.com>
2018-04-19 21:32:44 +0800

    + Add `MERGED_MISSIONS` for getter filter

--- store/modules/getters/mission.js
+++ store/modules/getters/mission.js
@@ -1,4 +1,19 @@
-export const getMissionList = state => state.missions;
+import { MERGED_MISSIONS } from '@/constant';
+
+export const getMissionList = (state) => {
+  const missions = [];
+  state.missions.forEach((m) => {
+    if (MERGED_MISSIONS[m.id]) {
+      console.log(m.id);
+      const target = MERGED_MISSIONS[m.id];
+      console.log(target);
+      /* dont push into return array if is merged mission */
+      if (state.missions.find(t => t.id === target)) return;
+    }
+    missions.push(m);
+  });
+  return missions;
+};
 
 export const getNewMissionlist = state => state.missions.filter(m => !m.seen);
 
