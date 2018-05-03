f28f7e8440f2a61e0a627536ed580cc2140a61a7
Miles Wong <miles@oice.com>
2018-05-03 10:56:53 +0800

    + Allow press articles to be opened automatically by query string

--- components/home/PressCoverage.vue
+++ components/home/PressCoverage.vue
@@ -24,7 +24,10 @@
         <div class="lc-container-3 lc-bg-gray-1">
           <div class="lc-container-4">
             <div class="lc-padding-top-24 lc-padding-bottom-32">
-              <image-grid :items="presses" lightboxId="presses" />
+              <image-grid
+                ref="imageGrid"
+                :items="presses"
+                lightboxId="presses" />
             </div>
           </div>
         </div>
@@ -41,56 +44,68 @@ import ImageGrid from '../ImageGrid';
 
 const presses = [
   {
+    id: 'bloomberg',
     src: '/images/press-logo/bloomberg.png',
     link: '/images/articles/20180502-bloomberg-businessweek.png',
     isLightBox: true,
     title: 'Bloomberg Businessweek, 2018.05.02',
   },
   {
+    id: 'cryptoground',
     src: '/images/press-logo/cryptoground.svg',
     link: 'https://www.cryptoground.com/a/likecoin-founder-kin-ko-presents-an-opportunity-for-artists-to-earn',
   },
   {
+    id: 'ejinsight',
     src: '/images/press-logo/ejinsight.png',
     link: 'http://www.ejinsight.com/20180409-hk-based-likecoin-uses-token-to-reward-online-content-creators/',
   },
   {
+    id: 'thestandard',
     src: '/images/press-logo/thestandard.svg',
     link: 'http://www.thestandard.com.hk/section-news.php?id=189734&story_id=47421669&d_str=20171117&sid=5',
   },
   {
+    id: 'blockcast',
     src: '/images/press-logo/blockcast.png',
     link: 'http://blockcast.it/2018/04/19/blockcast-interview-likecoin-founder-kin-ko/',
   },
   {
+    id: 'mrblock',
     src: '/images/press-logo/mrblock.png',
     link: 'https://www.youtube.com/watch?v=x4_-UY1baT8',
   },
   {
+    id: 'tvb',
     src: '/images/press-logo/tvb.svg',
     link: 'https://vimeo.com/261966523',
   },
   {
+    id: '881903',
     src: '/images/press-logo/881903.svg',
     link: '/images/articles/20180101-commercial-radio-hk.jpg',
     isLightBox: true,
     title: 'Commercial Radio Hong Kong, 2018.01.01',
   },
   {
+    id: 'mingpao',
     src: '/images/press-logo/ming-pao.svg',
     link: '/images/articles/20171007-mingbao.jpg',
     isLightBox: true,
     title: 'Mingpao (明報), 2017.10.07',
   },
   {
+    id: 'rthk',
     src: '/images/press-logo/rthk.svg',
     link: 'http://www.rthk.hk/radio/pth/programme/p0452_world_towards_informatio/episode/495605',
   },
   {
+    id: 'appledaily',
     src: '/images/press-logo/appledaily.jpg',
     link: 'https://video.appledaily.com.hk/mcp/encode/2018/03/05/3560694/20180304_fin_100AD_w.mp4',
   },
   {
+    id: 'inside',
     src: '/images/press-logo/inside.svg',
     link: 'https://www.inside.com.tw/2018/03/18/like-to-coin',
   },
@@ -105,6 +120,24 @@ export default {
     return {
       presses,
     };
+  },
+  mounted() {
+    const query = /[?&]press=([^&]*)/.exec(window.location.search);
+    if (query && query[1]) {
+      const imageId = query[1];
+
+      const press = this.presses.find(({ id }) => id === imageId);
+      if (imageId && press) {
+        const element = document.querySelector('#press');
+        if (element) element.scrollIntoView();
+
+        if (press.isLightBox) {
+          this.$nextTick(() => {
+            this.$refs.imageGrid.openLightBox(imageId);
+          });
+        }
+      }
+    }
   },
 };
 </script>
