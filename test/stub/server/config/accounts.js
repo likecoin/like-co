60a6a1537e5b94a893ace66eb12be73799de0005
Michael Cheung <mc.kingho@gmail.com>
2018-06-01 19:30:16 +0800

    + Add send like and eth unit test + Add delegator account for unit test

---
+++ test/stub/server/config/accounts.js
@@ -0,0 +1,10 @@
+const accounts = [
+  {
+    address: '0x08895B08f39B46B008276D8EAB6c5982a33edb31',
+    privateKey: '0x30007b3f2b58b3253abffc2d6692d1bfd3ab8c2e2ab80eff824da320166d13af',
+    gasPrice: '1000000000',
+    gasLimit: 900000,
+  },
+];
+
+module.exports = accounts;
