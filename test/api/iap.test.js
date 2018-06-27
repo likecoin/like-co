6cd07ed5325d041a2203dd9247599e90d9645aa7
William Chong <williamchong007@msn.com>
2018-06-27 17:38:36 +0800

    + Add basic iap test ! Fix missing unit test command in package.json

---
+++ test/api/iap.test.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import { url } from './data';
+
+const axiosist = require('axiosist');
+const app = require('../../build/main.js'); // eslint-disable-line import/no-unresolved
+
+test('IAP: Get iap product list', async (t) => {
+  const res = await axiosist(app).get(`${url}/api/iap/list`)
+    .catch(err => err.response);
+
+  t.is(res.status, 200);
+  t.deepEqual(res.data, []);
+});
