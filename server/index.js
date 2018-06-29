509c74d0974c507a9257a8409a8c1e99a671d2dc
Michael Cheung <mc.kingho@gmail.com>
2018-06-29 12:37:38 +0800

    * Fix x frame regex checking

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.set('port', port);
 if (process.env.NODE_ENV === 'production') app.disable('x-powered-by');
 
 app.use((req, res, next) => {
-  if (!/^\/in\/embed\/[a-z0-9-_]+$/.test(req.path)) {
+  if (!/^\/in\/embed\/[-a-z0-9_]+/.test(req.path)) {
     res.setHeader('X-Frame-Options', 'DENY');
   }
   res.setHeader('X-XSS-Protection', '1; mode=block');
