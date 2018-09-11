58e3960667286557f943541a1b7e293c27bd729f
Miles Wong <miles@like.co>
2018-09-11 10:29:33 +0800

    ! Fix unable to remove social link

--- components/settings/ExternalLinksPanel.vue
+++ components/settings/ExternalLinksPanel.vue
@@ -156,7 +156,7 @@ export default {
       }
     },
     async onRemoveLink({ id, order }) {
-      if (!order) {
+      if (order === undefined) {
         this.hasNewLink = false;
       } else {
         const index = this.links.findIndex(l => l.id === id);
