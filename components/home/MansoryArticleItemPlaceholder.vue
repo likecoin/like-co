cfb8978882fbd1b7cf984aff1dff957f7ca48345
Miles Wong <miles@like.co>
2018-09-12 11:14:40 +0800

    + Add suggestion list and statistics UI

---
+++ components/home/MansoryArticleItemPlaceholder.vue
@@ -0,0 +1,153 @@
+<template>
+  <article class="mansory-article-item-placeholder">
+    <div class="mansory-article-item-placeholder__wrapper">
+      <div class="mansory-article-item-placeholder__header">
+        <div class="mansory-article-item-placeholder__author">
+          <div class="background-masker mansory-article-item-placeholder__avatar" />
+          <div class="background-masker mansory-article-item-placeholder__name" />
+        </div>
+
+        <div class="background-masker mansory-article-item-placeholder__like-count" />
+      </div>
+
+      <div class="background-masker mansory-article-item-placeholder__og-image" />
+
+      <div class="mansory-article-item-placeholder__content">
+        <div class="background-masker mansory-article-item-placeholder__url" />
+
+        <div
+          :style="{ width: titleWidth }"
+          class="background-masker mansory-article-item-placeholder__title"
+        />
+
+        <div
+          v-if="isShowDescription"
+          class="mansory-article-item-placeholder__description-wrapper"
+        >
+          <div class="background-masker mansory-article-item-placeholder__description" />
+          <div
+            :style="{ width: descriptionLastLineWidth }"
+            class="background-masker mansory-article-item-placeholder__description"
+          />
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'mansory-article-item-placeholder-placeholder',
+  data() {
+    return {
+      titleWidth: `${50 + Math.random() * 45}%`,
+      isTitleTwoLines: Math.random() > 0.5,
+
+      isShowDescription: Math.random() > 0.5,
+      descriptionLastLineWidth: `${20 + Math.random() * 55}%`,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/variables";
+
+$author-avatar-size: 36px;
+
+.background-masker {
+  background-color: $gray-e6;
+}
+
+.mansory-article-item-placeholder {
+  break-inside: avoid;
+
+  img {
+    border: 1px solid $gray-e6;
+  }
+
+  &__wrapper {
+    border: 1px solid $gray-e6;
+    border-radius: 8px;
+    background-color: $like-white;
+  }
+
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 16px 24px 12px;
+
+    @media (max-width: 600px) {
+      padding-right: 16px;
+      padding-left: 16px;
+    }
+  }
+
+  &__author {
+    display: flex;
+    align-items: center;
+  }
+
+  &__avatar {
+    width: $author-avatar-size;
+    height: $author-avatar-size;
+    margin-right: 8px;
+
+    border-radius: 50%;
+  }
+
+  &__name {
+    width: 88px;
+    height: 24px;
+  }
+
+  &__like-count {
+    width: 72px;
+    height: 20px;
+  }
+
+  &__og-image {
+    height: 250px;
+
+    border: 1px solid $gray-e6;
+
+    @media (min-width: 600px + 1px) {
+      width: calc(100% + 16px);
+      max-width: unset;
+      margin-left: -8px;
+    }
+
+    @media (max-width: 600px) {
+      border-right: none;
+      border-left: none;
+    }
+  }
+
+  &__content {
+    padding: 8px 24px 16px;
+  }
+
+  &__url {
+    width: 60%;
+    height: 20px;
+    margin-bottom: 4px;
+  }
+
+  &__title {
+    width: 100%;
+    height: 26px;
+    margin-bottom: 2px;
+  }
+
+  &__description-wrapper {
+    margin-top: 8px;
+  }
+  &__description {
+    width: 100%;
+    height: 18px;
+    margin-top: 4px;
+  }
+}
+</style>
---
+++ components/home/RewardStatistics.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="reward-statistics">
+    <div class="reward-statistics__total-reward-wrapper">
+      <span class="lc-font-size-18">
+        {{ $t('Home.RewardStatistics.label.rewarded') }}
+      </span>
+      <h2
+        v-if="getTotalLIKERewardedStatistic"
+        class="reward-statistics__rewarded-amount-label"
+      >
+        {{ totalLIKEStrValue }}
+        <span class="reward-statistics__unit-label">LIKE</span>
+      </h2>
+      <div
+        v-if="getLikeCoinUsdNumericPrice && getTotalLIKERewardedStatistic"
+        class="reward-statistics__rewarded-amount-usd-label lc-color-gray-9b"
+      >(USD {{ totalLIKEUSDStrValue }})</div>
+    </div>
+
+    <div class="reward-statistics__details-wrapper lc-margin-top-32 lc-mobile">
+      <div
+        v-for="stat in stats"
+        :key="stat.title"
+      >
+        <span class="reward-statistics__stat-title">
+          {{ stat.title }}
+        </span>
+        <div class="reward-statistics__stat-content">
+          <div
+            v-if="stat.value !== '0'"
+            class="reward-statistics__stat-value"
+          >{{ stat.value }}</div>
+          <div
+            v-else
+            class="reward-statistics__stat-value-placeholder"
+          />
+          <div class="reward-statistics__stat-desc">
+            {{ stat.content }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'site-header',
+  props: {
+    isShowAccountButton: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    ...mapGetters([
+      'getLikeCoinUsdNumericPrice',
+      'getTotalLIKERewardedStatistic',
+      'getTotalLikerStatistic',
+      'getTotalLikeeStatistic',
+      'getTotalLIKEArticleStatistic',
+    ]),
+    stats() {
+      return [
+        {
+          title: this.$t('Home.RewardStatistics.label.from'),
+          content: this.$t('Home.RewardStatistics.label.readers'),
+          value: (this.getTotalLikerStatistic || 0).toLocaleString(),
+        },
+        {
+          title: this.$t('Home.RewardStatistics.label.to'),
+          content: this.$t('Home.RewardStatistics.label.creators'),
+          value: (this.getTotalLikeeStatistic || 0).toLocaleString(),
+        },
+        {
+          title: this.$t('Home.RewardStatistics.label.on'),
+          content: this.$t('Home.RewardStatistics.label.contents'),
+          value: (this.getTotalLIKEArticleStatistic || 0).toLocaleString(),
+        },
+      ];
+    },
+    totalLIKEStrValue() {
+      return Math.round(this.getTotalLIKERewardedStatistic || 0).toLocaleString();
+    },
+    totalLIKEUSDStrValue() {
+      return (
+        (this.getTotalLIKERewardedStatistic || 0) * this.getLikeCoinUsdNumericPrice
+      ).toFixed(2);
+    },
+  },
+  mounted() {
+    this.fetchLikeStatistic();
+  },
+  methods: {
+    ...mapActions([
+      'fetchLikeStatistic',
+    ]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/variables";
+
+.reward-statistics {
+  &__total-reward-wrapper {
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    @media (min-width: 600px + 1px) {
+      min-height: 145px;
+    }
+
+    > * {
+      color: $like-green;
+
+      font-weight: 600;
+    }
+  }
+
+  &__rewarded-amount-label {
+    animation: fade-in 0.35s ease-in;
+
+    font-size: 66px;
+
+    @media (max-width: 600px) {
+      font-size: 36px;
+    }
+  }
+
+  &__rewarded-amount-usd-label {
+    padding: 4px 16px;
+
+    animation: fade-in 0.35s ease-in;
+
+    border-bottom: 2px solid $like-gray-3;
+
+    @media (max-width: 600px) {
+      padding: 2px 0;
+
+      font-size: 12px;
+    }
+  }
+
+  &__unit-label {
+    position: absolute;
+    top: 54px;
+    right: 0;
+
+    font-size: 22px;
+
+    @media (max-width: 600px) {
+      top: 38px;
+
+      font-size: 16px;
+    }
+  }
+
+  &__details-wrapper {
+    display: flex;
+
+    > * {
+      flex: 1;
+    }
+
+    > div:first-child {
+      span {
+        color: $gray-9b;
+      }
+      .reward-statistics__stat-content {
+        color: $gray-9b;
+        border-left: 2px solid $gray-e6;
+      }
+    }
+  }
+
+  &__stat {
+    &-title {
+      color: $gray-9b;
+
+      font-size: 12px;
+      font-weight: 600;
+
+      @media (max-width: 600px) {
+        font-size: 10px;
+      }
+    }
+
+    &-content {
+      margin-top: 4px;
+      padding-bottom: 8px;
+
+      text-align: center;
+
+      color: $like-green;
+      border-right: 2px solid $gray-e6;
+
+      line-height: 1;
+    }
+
+    &-value {
+      animation: fade-in 0.35s ease-in;
+
+      font-size: 38px;
+
+      font-variant-numeric: oldstyle-nums;
+
+      @media (max-width: 600px) {
+        font-size: 20px;
+      }
+    }
+
+    &-desc {
+      margin-top: 12px;
+
+      font-size: 16px;
+
+      @media (max-width: 600px) {
+        font-size: 12px;
+      }
+    }
+  }
+
+  &__stat-value-placeholder {
+    min-height: 38px;
+  }
+}
+</style>
