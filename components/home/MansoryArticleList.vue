1b8f598b6591d59a0dcbee9b0d4dbd9bfa0808f4
Michael Cheung <mc.kingho@gmail.com>
2018-09-27 18:49:53 +0800

    * Update suggestion mansory to dedup

--- components/home/MansoryArticleList.vue
+++ components/home/MansoryArticleList.vue
@@ -63,9 +63,11 @@ export default {
   watch: {
     getSuggestedArticleInfo: {
       handler() {
+        const dedupByUser = new Set();
         this.articles = this.getSuggestedArticleList
           .filter(url => !!this.getSuggestedArticleInfo[url])
           .map(url => ({ ...this.getSuggestedArticleInfo[url], referrer: url }))
+          .filter(info => (dedupByUser.has(info.user) ? false : dedupByUser.add(info.user)))
           .slice(0, MAX_ARTICLES_DISPLAY);
       },
       deep: true,
