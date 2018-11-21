0f1f80e33d7bb8c89119533c335d3e2be3349583
Ng Wing Tat, David <i@ngwingt.at>
2018-11-21 18:09:42 +0800

    * Update /in page layout

--- pages/in/index.vue
+++ pages/in/index.vue
@@ -1,209 +1,207 @@
 <template>
   <div class="overview-page">
 
-    <div
-      v-if="isWallet && isEmailVerified"
-      id="earn"
-      class="bonus-container lc-container-0 lc-margin-top-48"
-    >
-      <section class="lc-container-1">
-        <div class="lc-container-header">
-          <div class="lc-container-2 lc-container-header-overlay">
-            <div class="lc-container-3 lc-bg-gray-1" />
+    <template v-if="!isEmailVerified">
+      <section class="lc-container-0 lc-margin-top-48">
+        <div class="lc-container-1">
+          <div class="lc-container-2">
+            <verify-email-cta :email-ref="'in'" />
           </div>
+        </div>
+      </section>
+    </template>
+
+    <!-- Show below if email is verified-->
+    <template v-else>
+      <section class="lc-container-0 lc-margin-top-48">
+        <div class="lc-container-1">
           <div class="lc-container-2">
             <div class="lc-container-3">
               <div class="lc-container-4">
-                <div class="lc-container-header-title">
-                  <h1 class="lc-font-size-32 lc-mobile">
-                    {{ $t('BonusPage.title') }}
-                  </h1>
-                  <div
-                    v-if="getIsFetchedMissions"
-                    class="lc-container-header-button-wrapper lc-mobile-hide"
-                  >
-                    <refresh-button
-                      :is-refreshing="getIsFetchingMissions"
-                      @click="refreshMissions"
-                    />
-                  </div>
-                </div>
-                <md-button
-                  class="md-likecoin lc-container-header-button lc-font-size-20"
-                  @click="$router.push({ name: 'in-bonus' })"
-                >
-                  {{ $t('BonusPage.button.moreBonus') }}
-                </md-button>
+                <like-button-intro-min />
               </div>
             </div>
           </div>
-        </div>
-
-        <div class="lc-container-2">
-
-          <div class="lc-padding-top-32 lc-bg-gray-1">
-            <mission-list
-              :missions="getShortMissionList"
-              :is-grid="false"
-              :is-loading="getIsFetchingMissions || !getIsFetchedMissions"
-              @click="onMissionClick"
-            />
-          </div>
-
-          <div class="lc-container-3 lc-padding-top-24 lc-bg-gray-1">
-            <div class="lc-container-4">
-              <div class="section-btn-container bonus-button-container lc-padding-bottom-32">
-                <md-button
-                  class="section-button md-likecoin"
-                  @click="$router.push({ name: 'in-bonus' })"
-                >
-                  {{ $t('BonusPage.button.moreBonus') }}
-                </md-button>
-              </div>
-            </div>
-          </div>
-
         </div>
       </section>
-    </div>
 
-    <!-- !! UI for user without wallet !! -->
-    <section
-      v-if="isEmailVerified"
-      class="lc-container-0"
-    >
-      <div class="lc-container-1">
-        <div class="lc-container-2">
-          <div class="lc-container-3">
-            <div class="lc-container-4">
-              <like-button-intro-min
-                v-if="!isWallet"
-              />
+      <section class="lc-container-0 lc-margin-top-48">
+        <!-- LikeCoin Bonus Overview -->
+        <div
+          v-if="isWallet"
+          id="earn"
+          class="lc-container-1 bonus-container"
+        >
+          <div class="lc-container-header">
+            <div class="lc-container-2 lc-container-header-overlay">
+              <div class="lc-container-3 lc-bg-gray-1" />
             </div>
-          </div>
-
-          <div
-            v-if="!isWallet"
-            class="
-              lc-container-3
-              lc-bg-gray-1
-              lc-container-no-padding-mobile
-              lc-margin-top-32
-              lc-padding-bottom-32"
-          >
-            <div
-              class="lc-container-4 lc-font-size-32 lc-font-weight-300 lc-mobile lc-padding-top-24"
-            >
-              {{ $t('In.mansory.title') }}
-              <!-- TODO: Temp hide refresh button until query updates
-              <div
-                class="lc-container-header-button-wrapper lc-mobile-hide lc-padding-top-24"
-              >
-                <refresh-button
-                  :is-refreshing="isFetchingLikeSuggestion"
-                  @click="refreshLikeSuggestion"
-                />
+            <div class="lc-container-2">
+              <div class="lc-container-3">
+                <div class="lc-container-4">
+                  <div class="lc-container-header-title">
+                    <h1 class="lc-font-size-32 lc-mobile">
+                      {{ $t('BonusPage.title') }}
+                    </h1>
+                    <div
+                      v-if="getIsFetchedMissions"
+                      class="lc-container-header-button-wrapper lc-mobile-hide"
+                    >
+                      <refresh-button
+                        :is-refreshing="getIsFetchingMissions"
+                        @click="refreshMissions"
+                      />
+                    </div>
+                  </div>
+                  <md-button
+                    class="md-likecoin lc-container-header-button lc-font-size-20"
+                    @click="$router.push({ name: 'in-bonus' })"
+                  >
+                    {{ $t('BonusPage.button.moreBonus') }}
+                  </md-button>
+                </div>
               </div>
-              -->
-            </div>
-            <div class="lc-container-4">
-              <mansory-article-list
-                class="lc-margin-vertical-24 lc-mobile"
-              />
-            </div>
-            <div class="lc-font-size-12 lc-font-weight-300 lc-text-align-center">
-              {{ $t('In.mansory.description') }}
-              <br>
-              <nuxt-link :to="{ name: 'in-earn' }">
-                {{ $t('In.mansory.descriptionCTA') }}
-              </nuxt-link>
             </div>
           </div>
+          <div class="lc-container-2">
 
-          <!-- TODO: Temp hide like button adopter (media using like button)
-          <div
-            v-if="!isWallet"
-            class="
-              lc-container-3
-              lc-bg-gray-1
-              lc-container-no-padding-mobile
-              lc-margin-top-32
-            "
-          >
-            <div class="lc-flex">
+            <div class="lc-padding-top-32 lc-bg-gray-1">
+              <mission-list
+                :missions="getShortMissionList"
+                :is-grid="false"
+                :is-loading="getIsFetchingMissions || !getIsFetchedMissions"
+                @click="onMissionClick"
+              />
+            </div>
+
+            <div class="lc-container-3 lc-padding-top-24 lc-bg-gray-1">
+              <div class="lc-container-4">
+                <div class="section-btn-container bonus-button-container lc-padding-bottom-32">
+                  <md-button
+                    class="section-button md-likecoin"
+                    @click="$router.push({ name: 'in-bonus' })"
+                  >
+                    {{ $t('BonusPage.button.moreBonus') }}
+                  </md-button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="lc-container-2 lc-mobile-show lc-margin-top-16 lc-margin-bottom-32">
+            <div class="lc-container-3 lc-flex lc-justify-content-center">
+              <refresh-button
+                :is-refreshing="getIsFetchingMissions"
+                :is-outline="true"
+                @click="updateInfo"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="lc-container-1 lc-margin-top-16">
+          <div class="lc-container-2">
+            <div
+              class="
+                lc-container-3
+                lc-bg-gray-1
+                lc-container-no-padding-mobile
+                lc-padding-bottom-32"
+            >
               <div
                 class="
                   lc-container-4
                   lc-font-size-32
                   lc-font-weight-300
                   lc-mobile
-                  lc-padding-top-24
-                  in-button-adopter__title
-                "
+                  lc-padding-top-24"
               >
-                {{ $t('In.likeButtonAdopter.title') }}
+                {{ $t('In.mansory.title') }}
+                <!-- TODO: Temp hide refresh button until query updates
+                <div
+                  class="lc-container-header-button-wrapper lc-mobile-hide lc-padding-top-24"
+                >
+                  <refresh-button
+                    :is-refreshing="isFetchingLikeSuggestion"
+                    @click="refreshLikeSuggestion"
+                  />
+                </div>
+                -->
               </div>
-              <div
-                class="
-                  lc-font-size-12
-                  lc-font-weight-300
-                  lc-padding-top-24
-                  lc-margin-top-16
-                  in-button-adopter__cta
-                "
-              >
+              <div class="lc-container-4">
+                <mansory-article-list
+                  class="lc-margin-vertical-24 lc-mobile"
+                />
+              </div>
+              <div class="lc-font-size-12 lc-font-weight-300 lc-text-align-center">
+                {{ $t('In.mansory.description') }}
+                <br>
                 <nuxt-link :to="{ name: 'in-earn' }">
-                  {{ $t('In.likeButtonAdopter.descriptionCTA') }}
+                  {{ $t('In.mansory.descriptionCTA') }}
                 </nuxt-link>
               </div>
             </div>
-            <like-button-adopter />
           </div>
-          -->
+        </div>
 
-          <!-- TODO: Temp hide before civic liker release
-          <div
-            v-if="!isWallet"
-            class="lc-margin-top-32"
-          >
+        <!-- TODO: LikeButton Adopter
+        <div class="lc-container-1 lc-margin-top-16">
+          <div class="lc-container-2">
+            <div
+              class="
+                lc-container-3
+                lc-bg-gray-1
+                lc-container-no-padding-mobile
+              "
+            >
+              <div class="lc-flex">
+                <div
+                  class="
+                    lc-container-4
+                    lc-font-size-32
+                    lc-font-weight-300
+                    lc-mobile
+                    lc-padding-top-24
+                    in-button-adopter__title
+                  "
+                >
+                  {{ $t('In.likeButtonAdopter.title') }}
+                </div>
+                <div
+                  class="
+                    lc-font-size-12
+                    lc-font-weight-300
+                    lc-padding-top-24
+                    lc-margin-top-16
+                    in-button-adopter__cta
+                  "
+                >
+                  <nuxt-link :to="{ name: 'in-earn' }">
+                    {{ $t('In.likeButtonAdopter.descriptionCTA') }}
+                  </nuxt-link>
+                </div>
+              </div>
+              <like-button-adopter />
+            </div>
+
+          </div>
+        </div>
+        -->
+
+      </section>
+
+      <!-- TODO: Civic Liker CTA
+      <section class="lc-container-0 lc-margin-top-48">
+        <div class="lc-container-1">
+          <div class="lc-container-2">
             <civic-liker-cta />
           </div>
-          -->
         </div>
-      </div>
-    </section>
-    <!-- !! UI for user without wallet !! -->
+      </section>
+      -->
 
-    <div
-      v-if="isEmailVerified"
-      class="lc-container-0 lc-margin-top-24 lc-mobile-show"
-    >
-      <div class="lc-container-1">
-        <div class="lc-container-2">
-          <div class="lc-container-3 lc-flex lc-justify-content-center">
-            <refresh-button
-              :is-refreshing="getIsFetchingMissions"
-              :is-outline="true"
-              @click="updateInfo"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div
-      v-if="!isEmailVerified"
-      class="lc-container-0 lc-margin-top-24"
-    >
-      <div class="lc-container-1">
-        <div class="lc-container-2">
-          <verify-email-cta
-            :email-ref="'in'"
-          />
-        </div>
-      </div>
-    </div>
-
+    </template>
   </div>
 </template>
 
