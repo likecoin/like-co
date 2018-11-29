e0a3988c76778ece4397c363d0ce9b15c52a580b
William Chong <williamchong007@msn.com>
2018-11-29 20:51:26 +0800

    + Add cors headers in `/users` for temp use with other service

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import cors from 'cors';
 
 import users from './users';
 import payment from './payment';
@@ -8,10 +9,15 @@ import oembed from './oembed';
 import social from './social';
 import { startPoller } from '../util/poller';
 import { ValidationError } from '../../util/ValidationHelper';
+import { IS_TESTNET, TEST_MODE } from '../../constant';
+
+const corsWhiteList = [/\.like\.co$/];
+if (IS_TESTNET || TEST_MODE) corsWhiteList.push(/^http(s)?:\/\/localhost(:\d+)?$/);
 
 const router = Router();
 
-// Add USERS Routes
+// HACK: allow users to be called from other services before api refactor
+router.use('/users', cors({ origin: corsWhiteList, credentials: true }));
 router.use(users);
 router.use(payment);
 router.use(iap);
