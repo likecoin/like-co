78829583e628da9313f9e48b3f3f0157ab110098
William Chong <williamchong007@msn.com>
2018-12-10 21:43:05 +0800

    (TEMP) ! Fix stack overflow error due to circular reference

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -4,7 +4,7 @@ import {
 
 export default function ({ store, route, redirect }) {
   if (!store.getters.getUserIsRegistered) {
-    store.commit(USER_SET_AFTER_AUTH_ROUTE, route);
+    if (!process.server) store.commit(USER_SET_AFTER_AUTH_ROUTE, route);
     redirect('/in/register');
   }
 }
