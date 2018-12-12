3cf37670b4345d533bcd3e21f5354f239370c721
Michael Cheung <mc.kingho@gmail.com>
2018-12-12 15:19:03 +0800

    + Add pre reg civic liker test

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -411,3 +411,22 @@ test('USER: Post user notitication option', async (t) => {
   t.is(res.status, 200);
   t.is(res.data.isEmailEnabled, true);
 });
+
+test('USER: Pre register civic liker campaign', async (t) => {
+  const user = testingUser1;
+  const token = jwtSign({ user });
+  let res = await axiosist.put(`/api/users/${user}/civic/trial`, { }, {
+    headers: {
+      Cookie: `likecoin_auth=${token}`,
+    },
+  }).catch(err => err.response);
+
+  t.is(res.status, 200);
+  res = await axiosist.get(`/api/users/id/${user}`, {
+    headers: {
+      Cookie: `likecoin_auth=${token}`,
+    },
+  });
+  t.is(res.status, 200);
+  t.is(res.data.isPreRegCivicLiker, true);
+});
