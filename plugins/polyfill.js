2932d1c0d2f6c8d916a9ada09f36911706623818
William Chong <williamchong007@msn.com>
2019-01-04 21:56:33 +0800

    + Add missing polyfill when upgraded to nuxt@2

---
+++ plugins/polyfill.js
@@ -0,0 +1,5 @@
+require('core-js/fn/object/assign');
+require('core-js/fn/object/values');
+require('core-js/fn/string/includes');
+require('core-js/fn/array/includes');
+require('classlist-polyfill');
