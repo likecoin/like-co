12187a7496adc3417de3468ea83f27e11172cc6c
William Chong <williamchong007@msn.com>
2019-01-28 20:08:51 +0800

    ! Fix authenticated middleware not working in test

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,15 +1,23 @@
 import {
   USER_SET_AFTER_AUTH_ROUTE,
 } from '@/store/mutation-types';
-import { EXTERNAL_HOSTNAME } from '@/constant';
 
-export default function ({ store, route, redirect }) {
+import {
+  TEST_MODE,
+} from '@/constant';
+
+export default function ({
+  req,
+  store,
+  route,
+  redirect,
+}) {
   if (!store.getters.getUserIsRegistered) {
     let redirectPath = '/in/register';
     if (!process.server) {
       store.commit(USER_SET_AFTER_AUTH_ROUTE, route);
     } else {
-      const targeturl = encodeURIComponent(`https://${EXTERNAL_HOSTNAME}${route.fullPath}`);
+      const targeturl = encodeURIComponent(`${TEST_MODE ? 'http' : 'https'}://${req.headers.host}${route.fullPath}`);
       redirectPath = `${redirectPath}?redirect=${targeturl}`;
     }
     redirect(redirectPath);
