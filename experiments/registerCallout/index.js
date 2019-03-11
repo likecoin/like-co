d0d62d2e99a85b0f5a2a4d0deca7cdc80f5e2497
William Chong <williamchong007@msn.com>
2019-03-11 16:32:32 +0800

    ⚗ End registerCallout experiment

--- experiments/registerCallout/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'register-callout',
-  experimentID: 'C2y4FL-nT_eCJBDYcaEc4g',
-  isEligible: ({ route }) => route.path.includes('register'),
-  variants: [
-    { name: 'original', weight: 1 },
-    { name: 'alternative', weight: 1 },
-  ],
-};
