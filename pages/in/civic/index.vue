6cd7e4daf30a838e2edf1456046d312120c425bd
William Chong <williamchong007@msn.com>
2019-04-17 18:07:23 +0800

    🚑 Fix syntax error

--- pages/in/civic/index.vue
+++ pages/in/civic/index.vue
@@ -3,8 +3,8 @@ import { IS_TESTNET } from '@/constant';
 
 export default {
   fetch({ redirect, query }) {
-    const queryString = Object.keys.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`.join('&'));
-    redirect(302, `https://${IS_TESTNET ? 'rinkeby' : ''}liker.land/civic?${queryString}`);
+    const queryString = Object.keys(query).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`.join('&'));
+    redirect(302, `https://${IS_TESTNET ? 'rinkeby.' : ''}liker.land/civic?${queryString}`);
   },
 };
 </script>
