0049118e3f728838947c6bdae1a9c34b4a0b525f
William Chong <williamchong007@msn.com>
2019-04-24 18:25:03 +0800

    💄 Use loading indicator in civic redirect page

--- pages/in/civic/index.vue
+++ pages/in/civic/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>{{$t('General.loading')}}</div>
+  <div class="lc-container-0">
+    <div class="lc-container-1">
+      <div class="lc-container-2">
+        <div class="lc-container-3">
+          <lc-loading-indicator />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { IS_TESTNET } from '@/constant';
 
 export default {
+  layout: 'blank',
   mounted() {
     const { query } = this.$route;
     const queryString = Object.keys(query).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`).join('&');
