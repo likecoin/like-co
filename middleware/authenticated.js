275f5ab64e6d99236d2cf7296f9a13624ef1aa80
William Chong <williamchong007@msn.com>
2019-04-26 19:35:02 +0800

    ✨ Support login flag in authenticate middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -10,15 +10,19 @@ export default function ({
   req,
   store,
   route,
+  query,
   redirect,
 }) {
   if (!store.getters.getUserIsRegistered) {
     let redirectPath = '/in/register';
+    const { login } = query;
     if (!process.server) {
       store.commit(USER_SET_AFTER_AUTH_ROUTE, route);
+      if (login === '1') redirectPath = `${redirectPath}?login=1`;
     } else {
       const targeturl = encodeURIComponent(`${TEST_MODE ? 'http' : 'https'}://${req.headers.host}${route.fullPath}`);
       redirectPath = `${redirectPath}?redirect=${targeturl}`;
+      if (login === '1') redirectPath = `${redirectPath}&login=1`;
     }
     redirect(redirectPath);
   }
