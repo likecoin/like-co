8c0aa66c0aa4f294bb136c2a510c0cee530e1620
Ng Wing Tat, David <i@ngwingt.at>
2019-05-03 17:02:22 +0800

    ✨ Add PlatformGrid component

---
+++ components/PlatformGrid.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="platform-grid">
+    <ul>
+      <li
+        v-for="item in items"
+        :key="item.id"
+        class="platform-grid__item"
+      >
+        <a
+          :href="item.url"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img v-lazy="item.logoSrc">
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'platform-grid',
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~assets/variables";
+
+.platform-grid {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    margin: -6px;
+    padding: 0;
+
+    list-style: none;
+  }
+
+  &__item {
+    flex: 25%;
+
+    padding: 6px;
+
+    @media (max-width: 600px) {
+      flex: 50%;
+    }
+
+    a {
+      overflow: hidden;
+
+      transition: transform 0.2s ease-in !important;
+
+      border: 1px solid $gray-e6;
+      border-radius: 16px;
+      background: white;
+
+      &:hover {
+        transform: translateY(-4px);
+      }
+    }
+
+    a, img {
+      display: block;
+    }
+
+    img {
+      object-fit: cover;
+    }
+  }
+}
+</style>
