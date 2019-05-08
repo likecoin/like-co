bc60e568ddb3cf338a824afe2e0b262c13129fda
William Chong <williamchong007@msn.com>
2019-05-08 20:10:59 +0800

    🔒 Add sameSite in CSRF cookie

--- components/LanguageSwitch.vue
+++ components/LanguageSwitch.vue
@@ -69,7 +69,7 @@ export default {
     ]),
     onChangeLanguage(locale) {
       this.$i18n.loadLanguageAsync(locale);
-      this.$cookie.set('language', locale, { expires: '1M', secure: true });
+      this.$cookie.set('language', locale, { expires: '1M', secure: true, samesite: 'none' });
       this.setLocale(locale);
     },
   },
