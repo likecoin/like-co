34d3a89c5bacd73fcf9194eb38649f4acdb73150
William Chong <williamchong007@msn.com>
2019-07-12 15:59:15 +0800

    ⚗ Update show avatar experiment

--- experiments/registerCallout/index.js
+++ experiments/registerCallout/index.js
@@ -1,6 +1,6 @@
 export default {
   name: 'register-form',
-  experimentID: 'aG4JuellQJOj3rssKR716g',
+  experimentID: 'xv-Zi0_kTua3YKvpAbJORQ',
   isEligible: ({ route }) => route.path.includes('register'),
   variants: [
     { name: 'original', weight: 1 },
