3c358617f6285d27ee4e7da9ae93d676fee9b201
William Chong <williamchong007@msn.com>
2019-08-06 01:12:22 +0800

    ✨ Add Matters login

--- util/api/api.js
+++ util/api/api.js
@@ -72,6 +72,8 @@ export const apiLoginUser = payload => axios.post('/users/login', payload);
 
 export const apiLogoutUser = () => axios.post('/users/logout');
 
+export const apiFetchAuthPlatformURL = platform => axios.get(`/users/login/${platform}`);
+export const apiPostAuthPlatformToken = (platform, { code, state }) => axios.post(`/users/login/${platform}`, { code, state });
 export const apiFetchLinkedAuthPlatforms = () => axios.get('/users/login/platforms');
 
 export const apiLinkAuthPlatform = (platform, payload) => axios.post(`/users/login/${platform}/add`, payload);
