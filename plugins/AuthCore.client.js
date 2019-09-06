9926c8e61ccfff421988ee04807d6f0d0e2c0c8a
William Chong <williamchong007@msn.com>
2019-09-06 17:10:06 +0800

    ✨ Add AuthCore re-auth logic and dialog

--- plugins/AuthCore.client.js
+++ plugins/AuthCore.client.js
@@ -1,6 +1,21 @@
-export default ({ store }) => {
+import { AuthCoreAuthClient } from 'authcore-js';
+
+import { AUTHCORE_API_HOST } from '@/constant';
+
+export default async ({ store }) => {
   if (window && window.localStorage) {
     const accessToken = window.localStorage.getItem('authcore.accessToken');
-    if (accessToken) store.dispatch('setAuthCoreToken', accessToken);
+    if (accessToken) {
+      store.dispatch('setAuthCoreToken', accessToken);
+      await new AuthCoreAuthClient({
+        apiBaseURL: AUTHCORE_API_HOST,
+        callbacks: {
+          unauthenticated: () => {
+            store.dispatch('setAuthCoreToken', '');
+          },
+        },
+        accessToken,
+      });
+    }
   }
 };
--- store/modules/getters/authCore.js
+++ store/modules/getters/authCore.js
@@ -1,5 +1,13 @@
 export const getAuthCoreAccessToken = state => state.accessToken;
 
+export const getAuthCoreNeedReAuth = (state, getters, rootState) => {
+  let cosmosWallet;
+  if (rootState.user.user) {
+    ({ cosmosWallet } = rootState.user.user);
+  }
+  return cosmosWallet && !state.accessToken;
+};
+
 export const getAuthCoreKVClient = state => state.kvClient;
 
 export const getAuthCoreCosmosAddress = state => state.cosmosProvider;
