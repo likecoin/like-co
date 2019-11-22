f83fddb3ff11226ee8b11c37af7d57fcae4497a3
William Chong <williamchong007@msn.com>
2019-11-22 20:30:54 +0800

    💄 Add missing getapp page

---
+++ pages/in/getapp.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>{{ $t('GetApp.title') }}</h1>
+    <div>
+      <a
+        :href="getLikerLandAppURL"
+        @click="onClickDownloadApp"
+      >
+        {{ $t('GetApp.button.downloadNow') }}
+      </a>
+    </div>
+    <div>
+      <a
+        :href="getLikerLandURL"
+        @click="onClickContinue"
+      >
+        {{ $t('GetApp.button.continueToWeb') }}
+      </a>
+    </div>
+    <div>
+      insert image here
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { getLikerLandAppURL, getLikerLandURL } from '@/util/api/api';
+import { checkIsMobileClient } from '~/util/client';
+import { logTrackerEvent } from '@/util/EventLogger';
+
+export default {
+  name: 'home',
+  computed: {
+    getLikerLandAppURL,
+    getLikerLandURL,
+  },
+  mounted() {
+    if (!checkIsMobileClient()) {
+      this.$router.push({ name: 'index' });
+    }
+  },
+  methods: {
+    onClickDownloadApp() {
+      logTrackerEvent(this, 'GetApp', 'ClickDownloadApp', 'ClickDownloadApp', 1);
+    },
+    onClickContinue() {
+      logTrackerEvent(this, 'GetApp', 'ClickContinueToWeb', 'ClickContinueToWeb', 1);
+    },
+  },
+};
+</script>
