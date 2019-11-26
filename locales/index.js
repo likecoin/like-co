bc217aa54e689a2e5280eb4b0a3b88f176ff2b28
William Chong <williamchong007@msn.com>
2019-11-26 19:28:19 +0800

    🔥 Remove non-maintained locales

--- locales/index.js
+++ locales/index.js
@@ -1,14 +1,6 @@
 import en from './client/en.json';
 // import zh from './zh.json';
 // import cn from './cn.json';
-// import ja from './ja.json';
-// import ko from './ko.json';
-// import de from './de.json';
-// import es from './es.json';
-// import fr from './fr.json';
-// import it from './it.json';
-// import pt from './pt.json';
-// import ru from './ru.json';
 
 export const defaultLocale = 'en';
 export const defaultMessage = {
@@ -19,14 +11,6 @@ export const supportedLocales = [
   'en',
   'zh',
   'cn',
-  'ja',
-  'ko',
-  'de',
-  'es',
-  'fr',
-  'it',
-  'pt',
-  'ru',
 ];
 
 export default supportedLocales;
