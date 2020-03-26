ccbc30ccd730a311a51471e8ca8ab1761d54c1fe
William Chong <williamchong007@msn.com>
2020-03-26 23:27:13 +0800

    📈 Add crisp sdk in background to collect event

---
+++ plugins/crisp.client.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
+
+if (process.env.CRISP_WEBSITE_ID) {
+  window.$crisp = [];
+  window.CRISP_WEBSITE_ID = process.env.CRISP_WEBSITE_ID;
+  const crisp = {
+    is: (...args) => window.$crisp.is(...args),
+    get: (...args) => window.$crisp.get(...args),
+    push: (...args) => window.$crisp.push(...args),
+    help: (...args) => window.$crisp.help(...args),
+  };
+
+  const d = document;
+  const s = d.createElement('script');
+  s.src = 'https://client.crisp.chat/l.js';
+  s.async = 1;
+  d.getElementsByTagName('head')[0].appendChild(s);
+
+  /* TODO: hide chat for now */
+  window.$crisp.push(['do', 'chat:hide']);
+
+  Vue.prototype.$crisp = crisp;
+}
--- util/EventLogger.js
+++ util/EventLogger.js
@@ -14,6 +14,76 @@ function digestMessage(message) {
   return window.crypto.subtle.digest('SHA-256', data);
 }
 
+export async function setUserSupportData(vue, u, { boot = false } = {}) {
+  const {
+    user,
+    intercomToken,
+    displayName,
+    email,
+    wallet,
+    cosmosWallet,
+    primaryPhone,
+    language,
+    isAuthCore,
+    LIKE,
+  } = u;
+  if (vue.$intercom) {
+    const opt = {};
+    if (boot) opt.LikeCoin = true;
+    if (user) opt.user_id = user;
+    if (intercomToken) opt.user_hash = intercomToken;
+    if (displayName) opt.name = displayName;
+    if (email) opt.email = email;
+    if (primaryPhone) opt.phone = primaryPhone;
+    if (wallet) opt.wallet = wallet;
+    if (cosmosWallet) opt.cosmos_wallet = cosmosWallet;
+    if (LIKE) opt.LIKE = LIKE;
+    if (language) opt.language = language;
+    if (isAuthCore) opt.binded_authcore = true;
+    vue.$intercom.update(opt);
+  }
+  if (window.$crisp) {
+    const { $crisp } = window;
+    if (displayName) $crisp.push(['set', 'user:nickname', [displayName]]);
+    if (email) $crisp.push(['set', 'user:email', [email]]);
+    if (primaryPhone) $crisp.push(['set', 'user:phone', [primaryPhone]]);
+    const opt = [];
+    if (boot) opt.push(['LikeCoin', true]);
+    if (wallet) opt.push(['wallet', wallet]);
+    if (cosmosWallet) opt.push(['cosmos_wallet', cosmosWallet]);
+    if (LIKE) opt.push(['LIKE', LIKE]);
+    if (language) opt.push(['language', language]);
+    if (isAuthCore) opt.push(['binded_authcore', true]);
+    window.$crisp.push(['set', 'session:data', [opt]]);
+  }
+}
+
+export function setUserSupportErrorEvent(vue, message) {
+  if (vue.$intercom) {
+    vue.$intercom.update({ lastError: message });
+    vue.$intercom.trackEvent('likecoin-store_error', { message });
+  }
+  if (window.$crisp) {
+    window.$crisp.push(['set', 'session:data', [[['lastError', message]]]]);
+    window.$crisp.push(['set', 'session:event', [[['likecoin-store_error', message]]]]);
+  }
+}
+
+export function setUserSupportOAuthFactors(vue, factors) {
+  if ((vue.$intercom || window.$crisp) && factors) {
+    const services = factors.map(f => f.service);
+    const intercomOpt = {};
+    const crispOpt = [];
+    services.forEach((service) => {
+      // eslint-disable-next-line no-param-reassign
+      if (service) intercomOpt[`binded_${service.toLowerCase()}`] = true;
+      if (service) crispOpt.push([`binded_${service.toLowerCase()}`, true]);
+    });
+    if (vue.$intercom) vue.$intercom.update(intercomOpt);
+    if (window.$crisp) window.$crisp.push(['set', 'session:data', [crispOpt]]);
+  }
+}
+
 export async function setTrackerUserId(userId) {
   if (window.doNotTrack || navigator.doNotTrack) return;
   window.dataLayer = window.dataLayer || [];
@@ -37,6 +107,9 @@ export function logTrackerEvent(
 ) {
   try {
     if (vue.$intercom) vue.$intercom.trackEvent(`likecoin-store_${action}`, { label });
+    if (window.$crisp) {
+      window.$crisp.push(['set', 'session:event', [[[`likecoin-store_${action}`, { label }]]]]);
+    }
     // do not track
     if (window.doNotTrack || navigator.doNotTrack) return;
     window.dataLayer = window.dataLayer || [];
