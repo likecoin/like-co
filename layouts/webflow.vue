67b5afccc9d1aab2f5b2f457e2eed7a96ad162c7
William Chong <williamchong007@msn.com>
2020-06-10 19:35:31 +0800

    🐛 Fix Webflow loading race condition in SPA to index page

--- layouts/webflow.vue
+++ layouts/webflow.vue
@@ -8,11 +8,28 @@
 export default {
   head() {
     return {
+      link: [
+        { rel: 'preload', href: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js', as: 'script' },
+        { rel: 'preload', href: '/vendor/webflow/js/main.js', as: 'script' },
+      ],
       script: [
-        { hid: 'jquery', src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js' },
-        { hid: 'webflow', src: '/vendor/webflow/js/main.js' },
+        {
+          hid: 'jquery',
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+          callback: () => {
+            this.loadWebflowScript();
+          },
+        },
       ],
     };
+  },
+  methods: {
+    loadWebflowScript() {
+      const script = document.createElement('script');
+      script.src = '/vendor/webflow/js/main.js';
+      script.type = 'text/javascript';
+      document.head.appendChild(script);
+    },
   },
 };
 </script>
