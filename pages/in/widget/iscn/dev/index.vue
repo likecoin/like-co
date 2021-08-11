f4105fbc011e3e6bbb2a0b623cf89005e2814e75
William <me@williamchong.cloud>
2021-08-11 18:39:07 +0800

    🐛 Fix wrong redirect path for iscn-dev widget

--- pages/in/widget/iscn/dev/index.vue
+++ pages/in/widget/iscn/dev/index.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   fetch({ redirect, params, query }) {
-    redirect(302, { name: 'in-in-widget-iscn', params, query });
+    redirect(302, { name: 'in-widget-iscn', params, query });
   },
 };
 </script>
