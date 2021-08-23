a8716cbd29c2c5d572136b22e8fb0af48658ea02
William Chong <me@williamchong.cloud>
2021-08-23 23:51:26 +0800

    🚸 Use better error message for wallet not found

--- pages/in/widget/pay/index.vue
+++ pages/in/widget/pay/index.vue
@@ -638,7 +638,7 @@ export default {
         const amount = new BigNumber(this.totalAmount);
         const from = await this.fetchCurrentCosmosWallet();
         if (!from) {
-          throw new Error('VALIDATION_FAIL');
+          throw new Error('PLEASE_RELOGIN');
         }
         const userWallet = cosmosWallet;
         if (from !== userWallet) {
