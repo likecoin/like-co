8f07667fcd5b1333cbcc7ab65c6145b81e5d1910
shellyyg <chih-ching.yang@whu.edu>
2022-01-07 11:59:18 +0800

    ♻️ Use @nuxt/components to move icon to components for clean code

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,6 +129,9 @@ const nuxtConfig = {
       },
     },
   },
+  buildModules: ['@nuxt/components'],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
   router: {
     middleware: 'sliding-menu',
