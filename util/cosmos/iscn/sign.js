6d74a4158ab46c0c7aeea25f6688c546fa4f1d4a
hsueh kuan tsai <b123456785678@gmail.com>
2022-05-02 15:21:20 +0800

    :recycle: check iscn-ar id only like prefix

--- util/cosmos/iscn/sign.js
+++ util/cosmos/iscn/sign.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import BigNumber from 'bignumber.js';
 import { ISCNSigningClient } from '@likecoin/iscn-js';
-
+import bech32 from 'bech32';
 import { ISCN_RPC_URL, ISCN_PUBLISHERS, ISCN_LICENSES } from './constant';
 import { EXTERNAL_URL } from '../../../constant';
 
@@ -13,6 +13,15 @@ function getISCNEstimationClient() {
     iscnClient = new ISCNSigningClient(ISCN_RPC_URL);
   }
   return iscnClient;
+}
+
+function changeAddressPrefix(address, newPrefix) {
+  const { words } = bech32.decode(`${address}`);
+  return bech32.encode(newPrefix, words);
+}
+
+function isValidLikeAddress(address) {
+  return /^like1[ac-hj-np-z02-9]{38}$/.test(address);
 }
 
 async function getISCNSigningClient(signer) {
@@ -69,9 +78,14 @@ function getPublisherISCNPayload(user, { publisher, license }) {
     }
   }
   if (author && cosmosWallet) { // from Keplr
+    let likePrefixAddress = cosmosWallet;
+    if (!isValidLikeAddress(cosmosWallet)) {
+      likePrefixAddress = changeAddressPrefix(cosmosWallet, 'like');
+    }
+
     stakeholders.unshift({
       entity: {
-        '@id': `did:cosmos:${cosmosWallet.split('cosmos')[1]}`,
+        '@id': `did:like:${likePrefixAddress.split('like')[1]}`,
         name: author,
         description: authorDescription,
       },
