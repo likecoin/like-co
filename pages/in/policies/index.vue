26f52bbbe6b07e0baf681c3e76f2e0fbe066772a
Ng Wing Tat, David <i@ngwingt.at>
2022-05-17 19:57:44 +0800

    🥅 Redirect policies pages to privacy

--- pages/in/policies/index.vue
+++ pages/in/policies/index.vue
@@ -1,50 +1,7 @@
-<template>
-  <div class="policies-page">
-    <section class="lc-container-0">
-
-      <div class="lc-container-1">
-        <div class="underlay lc-mobile-hide" />
-        <div class="lc-container-2">
-          <div class="lc-container-3 lc-padding-bottom-24">
-            <h1 class="lc-font-size-20 lc-font-weight-400 lc-line-height-1_5">
-              {{ $t('PoliciesPage.title' ) }}
-            </h1>
-          </div>
-        </div>
-      </div>
-
-      <div class="lc-container-1">
-        <div class="lc-container-2 lc-width-2-3">
-          <div class="lc-container-3 lc-padding-vertical-24">
-            <policies-links />
-          </div>
-        </div>
-      </div>
-
-    </section>
-  </div>
-</template>
-
-
 <script>
-import PoliciesLinks from '~/components/PoliciesLinks';
-
 export default {
-  name: 'policies-page',
-  components: {
-    PoliciesLinks,
-  },
-  head() {
-    return {
-      title: this.$t('PoliciesPage.title'),
-      meta: [
-        {
-          hid: 'og_title',
-          property: 'og:title',
-          content: this.$t('PoliciesPage.title'),
-        },
-      ],
-    };
+  fetch({ redirect, params, query }) {
+    redirect(301, { name: 'in-policies-privacy', params, query });
   },
 };
 </script>
