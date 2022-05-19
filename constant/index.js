d02bc1adfe61f38b44d2c37b6f071e1141b3cd80
Ng Wing Tat, David <i@ngwingt.at>
2022-05-19 19:13:13 +0800

    🚸 Redirect to /in/register after logout

--- constant/index.js
+++ constant/index.js
@@ -50,6 +50,7 @@ export const MEDIUM_REGEX = /^(?:https?:\/\/)?[^/]*\/media\/[a-zA-Z0-9_]+(?:\?po
 export const PUBSUB_TOPIC_MISC = 'misc';
 
 export const LOGIN_MESSAGE = 'Login - Reinventing the Like';
+export const POST_LOGOUT_ROUTE = '/in/register';
 
 export const ONE_LIKE = new BigNumber(10).pow(18);
 
