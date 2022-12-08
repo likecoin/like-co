40659d304454a35fb0b405c7bba86283be7a0791
William Chong <me@williamchong.cloud>
2022-12-08 16:07:16 +0800

    🔥 Remove about page

--- pages/in/about.vue
+++ pages/in/about.vue
@@ -1,178 +1,32 @@
 <template>
-  <div>
-    <section class="lc-container-0">
-      <div class="lc-container-1">
-        <div class="lc-container-2">
-          <header-flag />
-        </div>
-      </div>
-    </section>
-
-    <section class="lc-container-0 lc-header lc-mobile-hide">
-      <div class="lc-container-1">
-        <div class="underlay gray" />
-        <div class="lc-container-2">
-          <site-header :isShowAccountButton="true" />
-        </div>
-      </div>
-    </section>
-
-    <hero-video />
-
-    <section class="lc-container-0">
-      <div class="lc-container-1">
-        <div class="lc-container-2">
-          <div class="lc-container-3 lc-padding-vertical-48">
-            <div class="lc-container-4 lc-flex lc-justify-content-center lc-align-items-center">
-              <img
-                src="~/assets/about/wip.svg"
-                style="margin-right:16px"
-              >
-              <i18n
-                class="lc-color-like-gray-4 lc-font-size-16"
-                path="AboutPage.WIPNotice"
-                tag="span"
-              >
-                <a
-                  class="lc-color-like-green lc-font-weight-500"
-                  href="https://medium.com/likecoin/genesis-republic-of-liker-land-3903bd4d3bc6"
-                  place="moreAbout"
-                  target="_blank"
-                >{{ $t('AboutPage.moreAboutButtonText') }}</a>
-              </i18n>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <OfficialChannelList class="lc-margin-bottom-48" />
-
-    <ExchangeList class="lc-margin-vertical-48" />
-
-    <early-adopters class="lc-margin-top-48" />
-
-    <team class="lc-margin-vertical-48" />
-
+  <div class="about-redirect-page">
+    <Spinner :size="56" />
   </div>
 </template>
-
-
 <script>
-import { mapGetters } from 'vuex';
-
-import SiteHeader from '~/components/header/HeaderWithMenuButton';
-import HeroVideo from '~/components/about/HeroVideo';
-import HeaderFlag from '~/components/about/Flag';
-import EarlyAdopters from '~/components/about/EarlyAdopters';
-import ExchangeList from '@/components/home/ExchangeList';
-import OfficialChannelList from '@/components/home/OfficialChannelList';
-import Team from '~/components/about/Team';
-
-const images = require.context('../../assets/home');
-const imgUrl = path => images(`./${path}`);
+import Spinner from '~/components/Spinner';
 
 export default {
-  name: 'home',
-  layout: 'index',
+  layout: 'blank',
   components: {
-    EarlyAdopters,
-    ExchangeList,
-    HeaderFlag,
-    HeroVideo,
-    OfficialChannelList,
-    SiteHeader,
-    Team,
-  },
-  computed: {
-    ...mapGetters([
-      'getUserInfo',
-      'getUserIsRegistered',
-    ]),
-  },
-  head() {
-    return {
-      title: this.$t('TokenSale.button.aboutLikeCoin'),
-      meta: [
-        {
-          hid: 'og_title',
-          property: 'og:title',
-          content: this.$t('TokenSale.button.aboutLikeCoin'),
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$t('Home.Flag.content'),
-        },
-        {
-          hid: 'og_description',
-          property: 'og:description',
-          content: this.$t('Home.Flag.content'),
-        },
-        {
-          hid: 'og_image',
-          property: 'og:image',
-          content: 'https://like.co/images/og/sale.png',
-        },
-      ],
-    };
+    Spinner,
   },
   mounted() {
-    const { hash } = document.location;
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) element.scrollIntoView();
-    }
-  },
-  methods: {
-    imgUrl,
+    window.location.replace('https://about.like.co');
   },
 };
 </script>
 
-
 <style lang="scss" scoped>
-@import "~assets/variables";
+.about-redirect-page {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 
-$carousel-height: 488px;
-
-.lc-community {
-  font-size: 20px;
-
-  .lc-community-content {
-    display: flex;
-    flex-direction: row;
-
-    > div {
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-
-      &.platforms {
-        flex: 2;
-
-        > span {
-          margin-right: 16px;
-        }
-      }
-
-      &.faq {
-        flex: 1;
-        justify-content: center;
-      }
-    }
-  }
-}
-
-.cta-section.bottom {
-  @media (min-width: 600px) {
-    margin-top: 64px;
-  }
-}
-
-.social-links-container-mobile {
   display: flex;
+  align-items: center;
   justify-content: center;
 }
-
 </style>
