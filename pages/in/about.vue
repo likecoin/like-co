34e71dc29d63cfaa7885c21fcf055798e138fd2e
William Chong <me@williamchong.cloud>
2025-12-17 05:02:58 +0800

    🔍️ Use 301 redirect for legacy URLs

--- pages/in/about.vue
+++ pages/in/about.vue
@@ -1,32 +1,7 @@
-<template>
-  <div class="about-redirect-page">
-    <Spinner :size="56" />
-  </div>
-</template>
 <script>
-import Spinner from '~/components/Spinner';
-
 export default {
-  layout: 'blank',
-  components: {
-    Spinner,
-  },
-  mounted() {
-    window.location.replace('https://about.like.co');
+  fetch({ redirect }) {
+    redirect(301, 'https://about.like.co');
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.about-redirect-page {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
